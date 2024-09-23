7e97b15b017f0d5b77ecbc7af2e513059cd8c812
HeitorAzevedoBelo <heitorazevedo5511@gmail.com>
2024-09-22 22:09:57 -0300

    change dolar source

--- rpa.py
+++ rpa.py
@@ -14,14 +14,14 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.google.com/search?q=dolar')
+driver.get('https://www.remessaonline.com.br/cotacao/cotacao-dolar')
 
 # Configurando WebDriverWait
 wait = WebDriverWait(driver, 10)  # Aguarda até 10 segundos
 
 # Capturando valor do dólar com espera explícita
-dolar_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')))
-dolar = float(dolar_element.text.replace(',', '.'))
+dolar_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[2]/div/div[1]/div/div[1]')))
+dolar = float(dolar_element.text.split(' ')[0].replace(',', '.'))
 
 # Capturando data e hora
 data = datetime.now().strftime('%Y-%m-%d')
